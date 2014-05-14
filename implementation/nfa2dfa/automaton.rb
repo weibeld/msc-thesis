0ea20a0aa7bcbc1b8e4dfdce2ae520503fb247c8
Daniel Weibel <daniel.weibel@unifr.ch>
2014-05-14 16:16:38 +0200

    Started rewriting the Ruby automaton backend
    New implementation in automaton.rb (old: nfa2dfa.rb).

--- implementation/nfa2dfa/automaton.rb
+++ implementation/nfa2dfa/automaton.rb
@@ -3,18 +3,145 @@ require 'nokogiri'
 require 'set'
 require 'graphviz' # Requires installation of gem ruby-graphviz and of Graphviz
 
-class DFA
-    attr_accessor :states, :alphabet, :transitions, :start_state, :accept_states    
+class Automaton
+  attr_accessor :states, :alphabet, :transitions, :start_state, :accept_states
 
-    def initialize(file=nil)
-        if file == nil then return end
-        yaml = YAML::load_file(file)
-        @states = yaml['states']
-        @alphabet = yaml['alphabet']
-        @transitions = format_transitions(yaml['transitions'])
-        @start_state = yaml['start_state']
-        @accept_states = yaml['accept_states']
+  # Creates an automaton either from an initilization hash or a YAML string.
+  def initialize(arg)
+    if arg.is_a?(Hash)
+      @states = arg['states']
+      @alphabet = arg['alphabet']
+      @transitions = arg['transitions']
+      @start_state = arg['start_state']
+      @accept_states = arg['accept_states']
+    elsif arg.is_a?(String)
+      yaml = YAML::load(arg)
+      @states = yaml['states']
+      @alphabet = yaml['alphabet']
+      @transitions = machinize_transitions(yaml['transitions'])
+      @start_state = yaml['start_state']
+      @accept_states = yaml['accept_states']
+    end        
+  end
+
+  # Returns YAML string of automaton.
+  def to_yaml
+    h = Hash.new
+    h['states'] = @states
+    h['alphabet'] = @alphabet
+    h['start_state'] = @start_state
+    h['accept_states'] = @accept_states
+    h['transitions'] = humanize_transitions(@transitions)
+    h.to_yaml
+  end
+
+  # Returns true if automaton is determinstic and false if automaton is
+  # non-deterministic.
+  def is_deterministic
+    @states.each do |state|
+      @alphabet.each do |symbol|
+        if transition_function(state, symbol).size > 1 then return false end
+      end
     end
+    return true
+  end
+
+  
+  def accept?(word)
+    tmp_file = to_goal_file
+    # goal input returns either:
+    #   (true, s0 s2 s4 { s1 })
+    #   (false, null)
+    #   The inpute sequence is invalid.
+    result = `goal #{tmp_file} input #{word}`
+    if result.include?("true") then return true
+    elsif result.include?("false") then return false
+    else return false
+    end 
+    `rm #{tmp_file}`
+  end
+
+  
+  # Generates and returns a random automaton.
+  def self.generate_random
+  end
+  
+  
+
+private
+
+  # Returns a new automaton object from a GOAL file string.
+  def self.create_new_from_goal(string)
+    
+  end
+
+  # Creates a temporary GOAL file of the automaton and returns the filename.
+  def to_goal_file
+  end
+
+
+  # Returns the set of new states when the automaton is in state <state> and
+  # reads symbol <symbol>. The returned set may contain 0, 1, or more states.
+  def transition_function(state, symbol)
+    set = Set.new
+    if !@transitions[state] return set
+    @transitions[state].each do |successor, symbols|
+      if symbols.include? symbol then set << successor end
+    end
+    set
+  end
+
+  # Transform
+  #   {"p" => {"q" => Set{"a","b"}, "r" => Set{"a"}},
+  #    "q" => {"r" => Set{"b"}}}
+  # into
+  #   ["p -> q: a,b",
+  #    "p -> r: a",
+  #    "q -> r: b"]
+  def humanize_transitions(hash)
+    transitions = []
+    hash.each do |from, tos|
+      tos.each do |to, symbol_set|
+        string = "#{from} -> #{to}: "
+        symbol_set.each do |symbol|
+          string << "#{symbol},"
+        end
+        transitions << string.chomp(',')
+      end
+    end
+    transitions
+  end
+
+  # Transform
+  #   ["p -> q: a,b",
+  #    "p -> r: a",
+  #    "q -> r: b"]
+  # into
+  #   {"p" => {"q" => Set{"a","b"}, "r" => Set{"a"}},
+  #    "q" => {"r" => Set{"b"}}}
+  def machinize_transitions(array)
+      transitions = {}
+      array.each do |edge|
+          states = edge.split(":")[0].split("->")
+          from = states[0].strip
+          to = states[1].strip
+          symbols = edge.split(":")[1].split(",").each { |sym| sym.strip! }.to_set
+          if transitions[from]
+              if transitions[from][to]
+                  transitions[from][to].merge symbols
+              else
+                  transitions[from][to] = symbols
+              end
+          else
+              transitions[from] = {to => symbols}
+          end
+      end
+      transitions
+  end
+end
+
+class DFA
+    
 
     def accept?(string)
         current_state = @start_state
@@ -22,15 +149,7 @@ class DFA
         @accept_states.include? current_state
     end
 
-    def to_yaml(filename='dfa.yaml')
-        h = Hash.new
-        h['states'] = @states
-        h['alphabet'] = @alphabet
-        h['start_state'] = @start_state
-        h['accept_states'] = @accept_states
-        h['transitions'] = @transitions
-        File.open(filename, 'w') { |file| file.write h.to_yaml }
-    end
+    
 
     def visualize(filename="dfa.svg")
         visualize_automaton self, filename
@@ -40,16 +159,7 @@ end
 class NFA
     public
 
-    attr_accessor :states, :alphabet, :transitions, :start_state, :accept_states
-
-    def initialize(file)
-        yaml = YAML::load_file(file)
-        @states = yaml['states']
-        @alphabet = yaml['alphabet']
-        @transitions = format_transitions(yaml['transitions'])
-        @start_state = yaml['start_state']
-        @accept_states = yaml['accept_states']
-    end          
+    attr_accessor :states, :alphabet, :transitions, :start_state, :accept_states         
 
     def accept?(word)
         current_states = Set[@start_state]
@@ -100,15 +210,7 @@ class NFA
         visualize_automaton self, filename
     end
 
-    def transition_function(state, symbol)
-        s = Set.new
-        if @transitions[state]
-            @transitions[state].each do |successor, symbols|
-                if symbols.include? symbol then s.add successor end
-            end
-        end
-        s
-    end
+    
 end
 
 class Set
@@ -119,32 +221,7 @@ class Set
     end
 end
 
-# Transform
-#   ["p -> q: a,b",
-#    "p -> r: a",
-#    "q -> r: b"]
-# into
-#   {"p" => {"q" => Set{"a","b"}, "r" => Set{"a"}},
-#    "q" => {"r" => Set{"b"}}}
-def format_transitions(array)
-    transitions = {}
-    array.each do |edge|
-        states = edge.split(":")[0].split("->")
-        from = states[0].strip
-        to = states[1].strip
-        symbols = edge.split(":")[1].split(",").each { |sym| sym.strip! }.to_set
-        if transitions[from]
-            if transitions[from][to]
-                transitions[from][to].merge symbols
-            else
-                transitions[from][to] = symbols
-            end
-        else
-            transitions[from] = {to => symbols}
-        end
-    end
-    transitions
-end
+
 
 def visualize_automaton(automaton, filename)
     g = GraphViz.new :DFA, :type => :digraph
