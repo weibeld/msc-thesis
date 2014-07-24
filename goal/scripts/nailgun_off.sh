ade269ed55ebddcb2974fb2a42d1b11471c76612
Daniel Weibel <daniel.weibel@unifr.ch>
2014-07-24 11:49:26 +0200

    Nailgun script (working)
    Adapted the script of the web app for executing GOAL command line
    commands with Nailgun. Until today, it didn' work for GOAL commands that
    take a GFF file as argument. Today it suddenly worked. I don't know what
    the change is that made it work.
    
    The execution time of GOAL command line commands can be reduced by 2-2.5
    seconds. However, it doesn't affect the startup time of the GOAL GUI by
    clicking on the application icon or on a GFF file.
    
    One thing that doesn't work with Nailgun enabled is to start the GOAL
    GUI from the command line (running 'goal'). The GOAL GUI can be started
    and used, but when one tries to close it, it doesn't respond to
    anything.

---
+++ goal/scripts/nailgun_off.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Reverts the changes made by nailgun_on.sh. In particular, stops the Nailgun
+# server and reverts the goal script to the original one.
+#
+# Usage: ./nailgun_off.sh
+#
+# Daniel Weibel, 23.07.2014
+
+if [ $# -gt 0 ]; then
+  echo "Usage:"
+  echo "    $(basename $0)"
+  echo " Description:"
+  echo "    Reverts the changes made by nailgun_on.sh. In particular, stops the Nailgun"
+  echo "    server, and reverts the goal script ('which goal') to the original one."
+  exit 0
+fi
+
+GOAL_DIR=$(dirname $(which goal))
+BACKUP=goal.original
+
+# If no Nailgun server is running
+if [ "$(ps -e | grep NGServe[r])" = "" ]; then
+  echo "No Nailgun server is running"
+else
+  echo "Stopping Nailgun server..."
+  ng ng-stop
+fi
+
+if [ -f $GOAL_DIR/$BACKUP ]; then
+  echo "Restoring original goal script ('which goal')..."
+  mv $GOAL_DIR/$BACKUP $GOAL_DIR/goal
+fi
