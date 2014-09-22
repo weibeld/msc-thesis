511385f374bad0b50b323ed37ae0cbf0c2ceab11
Daniel Weibel <daniel.weibel@unifr.ch>
2014-09-22 18:17:38 +0200

    Implemented most of the todo's
    Not yet thoroughly tested.

--- bin/goal/complement.sh
+++ bin/goal/complement.sh
@@ -2,10 +2,9 @@
 # dw-14.09.2014
 
 # Frequently edited parameters
-data=~/data/test_set_s15
-#data=~/Desktop/automata
-goal=~/bin/GOAL-20140808/goal
-#goal=goal
+local=false
+if [ "$local" == true ]; then data=~/Desktop/automata; else data=~/data/test; fi
+goal=goal # Assumes 'goal' is in PATH on cluster node
 algo=fribourg
 opts="-r2ifc -m"
 timeout=600 # Seconds
@@ -22,29 +21,26 @@ if [ ! -f $goal ] && [ -z "$(which $goal)" ]; then
 out=out
 log=log
 
+# Out file format
+na=NA  # Default string for R's NA ("not available")
+yes=Y; no=N # Signaling timeout and memory-out
+sep="\t"
+
 # Nice date
 d() { date "+%F %H:%M:%S"; }
 
-# GNU time
-time=~/bin/gnu_time/bin/gtime
-#time=~/Desktop/time/bin/time
-time_format="real: %e\nuser_cpu: %U\nsys_cpu: %S\n"
-
-# Out file format
-na=NA  # Default string for R's NA ("not available")
-sep="\t"
-
 # Out file initialisation
-echo    "# $(d)" >>$out
-echo -e "# Complementation:\t\t$algo $opts" >>$out
-echo -e "# Memory limit (Java heap):\t$memory" >>$out
-echo -e "# Timeout (CPU time):\t\t${timeout}s" >>$out
-echo -e "states${sep}t_out${sep}m_out${sep}real_t${sep}cpu_t${sep}file" >>$out
+echo -e "# Starting date:\t\t$(d)" >>$TMP/$out
+echo -e "# Cluster node:\t\t\t$(uname -n)" >>$TMP/$out   
+echo -e "# Complementation algorithm:\t$algo $opts" >>$TMP/$out
+echo -e "# Memory limit (Java heap):\t$memory" >>$TMP/$out
+echo -e "# Timeout (CPU time):\t\t${timeout}s" >>$TMP/$out
+echo -e "states${sep}t_out${sep}m_out${sep}real_t${sep}ucpu_t${sep}scpu_t${sep}tcpu_t${sep}dt${sep}da${sep}file" >>$TMP/$out
 
 # Log to log file, or log file and screen
 log() {
-  if [ $log_stdout == true ]; then msg "$@" | tee -a $log
-  else msg "$@" >>$log; fi
+  if [ $TMP/$log_stdout == true ]; then msg "$@" | tee -a $TMP/$log
+  else msg "$@" >>$TMP/$log; fi
 }
 msg() {
   if [ "$2" != "" ]; then echo -n "$1"
@@ -58,28 +54,37 @@ export JVMARGS="-Xmx$memory"
 ctrl_c() { exit 1; }
 trap ctrl_c SIGINT
 
-tmp=tmp.out
+# Pack folder holding data and move it to local scratch
+tar -cz -f tmp.tar.gz -C $(dirname $data) $(basename $data)
+mv tmp.tar.gz $TMP
+tar -xz -f $TMP/tmp.tar.gz -C $TMP && rm $TMP/tmp.tar.gz
+data=$TMP/$(basename $data)
+
+tmp=$TMP/tmp.out
 for filename in $data/*.gff; do
   file=$(basename $filename)
 
-  # Complementation
+  # Complementation command execution
+  # ----------------------------------------------------------------------------
   log "$(d): Complementing $file... " n
   ulimit -S -t $timeout
-  { $time -f "$time_format" $goal complement -m $algo $opts $filename; } &>$tmp
+  { time $goal complement -m $algo $opts $filename; } &>$tmp
   #timeout ${timeout}s $goal complement -m fribourg $filename &>$tmp # Exit code 124
   ulimit -S -t unlimited
 
-  t_out=0
-  m_out=0
+  # States, timeout, memory-out
+  # ----------------------------------------------------------------------------
+  t_out=$no
+  m_out=$no
   # Timeout
   if grep -q "time limit exceeded" $tmp; then
     log "Timeout"
-    t_out=1
+    t_out=$yes
     states=$na
   # Memory excess
   elif grep -q "java.lang.OutOfMemoryError" $tmp; then
     log "Memory excess"
-    m_out=1
+    m_out=$yes
     states=$na
   # Successful execution
   else
@@ -88,13 +93,30 @@ for filename in $data/*.gff; do
   fi
 
   # Times
-  wallclock=$(grep '^real' $tmp | cut -d " " -f 2)
-  user_cpu=$(grep '^user' $tmp | cut -d " " -f 2)
-  sys_cpu=$(grep '^sys' $tmp | cut -d " " -f 2)
-  total_cpu=$(bc <<< "$user_cpu + $sys_cpu")
+  # ----------------------------------------------------------------------------
+  # Convert a line of the UNIX time output to seconds. The single argument must
+  # be ONE STRING containig a substring of the form "4m54.234s".
+  seconds() {
+    m=$(echo "$1" | egrep -o '[0-9]*m' | sed 's/m$//')
+    s=$(echo "$1" | egrep -o '[0-9][0-9]?.[0-9]*s' | sed 's/s$//')
+    seconds=$(bc <<< "$m * 60 + $s")
+    printf "%.3f" $seconds # printf performs rounding
+  }
+  real=$(seconds "$(grep '^real' $tmp)")
+  user_cpu=$(seconds "$(grep '^user' $tmp)")
+  sys_cpu=$(seconds "$(grep '^sys' $tmp)")
+  tot_cpu=$(bc <<< "$user_cpu + $sys_cpu")
+
+  # Transition and acceptance density
+  # ----------------------------------------------------------------------------
+  # Assumes filename of the form "s15_t1.0_a0.1_001.gff"
+  dt=$(echo $file | egrep -o "t[0-9].[0-9]" | sed 's/^t//')
+  da=$(echo $file | egrep -o "a[0-9].[0-9]" | sed 's/^a//')
 
   # Write out file
-  echo -e ${states}${sep}${t_out}${sep}${m_out}${sep}${wallclock}${sep}${total_cpu}${sep}${file} >>$out
+  echo -e ${states}${sep}${t_out}${sep}${m_out}${sep}${real}${sep}${user_cpu}${sep}${sys_cpu}${sep}${tot_cpu}${sep}${dt}${sep}${da}${sep}${file} >>$TMP/$out
 
 done
-rm $tmp
+
+# Copy result files to the global job directory
+cp $TMP/$out $TMP/$log .
--- bin/ubelix/submit.sh
+++ bin/ubelix/submit.sh
@@ -10,6 +10,8 @@ usage() {
   echo "    -c: CPU time            [01:00:00]"
   echo "    -o: STDOUT file         [<job>.out]"
   echo "    -e: STDERR file         [<job>.err]"
+  echo "    -s: Scratch size        [2G]         (set to 0 if no scratch desired)"
+  echo "    -p: PE SMP slots        [2]          (set to 0 if no PE desired)"
   echo "    -n: E-mail notification [a]"
   echo "        b: E-mail is sent at beginning of job"
   echo "        e: E-mail is sent at end of job"
@@ -18,19 +20,21 @@ usage() {
   echo "        n: No e-mails are sent"
 }
 
-while getopts ":m:c:r:o:e:n:" opt; do
+while getopts ":m:c:r:o:e:s:p:n:" opt; do
   case $opt in
     m) memory=$OPTARG; ;;
     c) cpu_time=$OPTARG; ;;
     o) out_file=$OPTARG; ;;
     e) err_file=$OPTARG; ;;
+    s) scratch_size=$OPTARG; ;;
+    p) slots=$OPTARG; ;;
     n) notification=$OPTARG; ;;
     \?)
-      echo "Error: invalid option: -$OPTARG."
+      echo "Error: invalid option: -$OPTARG"
       exit 1
       ;;
     :)
-      echo "Error: option -$OPTARG requires an argument."
+      echo "Error: option -$OPTARG requires an argument"
       exit 1
       ;;
   esac
@@ -43,6 +47,8 @@ if [ $# -ne 1 ]; then
 fi
 
 script=$1
+if [ ! -f $script ]; then echo "Error: $script is not a valid file"; exit 1; fi
+
 job=$(basename $(dirname $script))
 
 if [ -z $memory ]; then memory=4G; fi
@@ -51,15 +57,32 @@ if [ -z $out_file ]; then out_file=$job.out; fi
 if [ -z $err_file ]; then err_file=$job.err; fi
 if [ -z $notification ]; then notification=a; fi
 
+if [ -z $scratch_size ]; then scratch_size=2G; fi
+if [ $scratch_size == 0 ]; then scratch=""
+else scratch="-l scratch=1,scratch_size=$scratch_size,scratch_files=1M "; fi
+
+if [ -z $slots ]; then slots=2; fi
+if [ $slots == 0 ]; then pe=""
+else pe="-pe smp $slots -R y "; fi
+
 email=daniel.weibel@unifr.ch
 
 cd $(dirname $script)
 
-qsub -cwd \
-     -M $email \
-     -m $notification \
-     -l h_cpu=$cpu_time,h_vmem=$memory \
-     -o $out_file \
-     -e $err_file \
-     -N $job \
-     $(basename $script)
+cmd=\
+"qsub "\
+"-cwd "\
+"-V "\
+"-M $email "\
+"-m $notification "\
+"-l h_cpu=$cpu_time,h_vmem=$memory "\
+"$scratch"\
+"$pe"\
+"-o $out_file "\
+"-e $err_file "\
+"-N $job "\
+"$(basename $script)"
+
+$cmd
+
+echo $cmd >qsub
