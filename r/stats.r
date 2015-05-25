29153f7bedd66617aae1782bb44d4c663bef18de
Daniel Weibel <daniel.weibel@unifr.ch>
2015-05-25 20:47:40 +0200

    More analysis facilities; started some additional runs on UBELIX

--- r/stats.r
+++ r/stats.r
@@ -16,7 +16,7 @@ Read <- function(file) {
 ReadSubdirs <- function() {
   # Read all the CSV files in all the subdirectories of the current directory
   # into separate data frames.
-  # Returns: a list of data frames, named after the input file names
+  # Returns: a list of data frames, with elements named after the input files
   #----------------------------------------------------------------------------#
   l <- list()
   for (f in Sys.glob("*/*.csv")) {
@@ -38,86 +38,197 @@ OutM_ <- function(df) { df[df$memout == "Y",] }
 Out   <- function(df) { nrow(Out_(df)) }
 Out_  <- function(df) { df[df$timeout == "Y" | df$memout == "Y",] }
 
-
-StatsAgg <- function(df, col="states", v="") {
-  # Get statistics aggregated over all automata
+Outs <- function(list, labels=names(list)) {
+  # Get number of timeouts and memory outs for each of a list of data frames
+  # Args: list: a list of data frames
+  #       labels: character vector with a label for each data frame in 'list'
+  # Returns: a data frame with one row for each data frame in 'list'
   #----------------------------------------------------------------------------#
-  s <- df[!is.na(df$states), col]
-  data.frame(Version = v,
-             Mean    = as.numeric(mean(s)),
-             Min     = as.integer(min(s)),
-             P25     = as.numeric(quantile(s, 0.25)),
-             Median  = as.numeric(median(s)),
-             P75     = as.numeric(quantile(s, 0.75)),
-             Max     = as.integer(max(s)))
+  i <- 1
+  for (df in list) {
+    row <- data.frame(Version = labels[i],
+                      Time    = OutT(df),
+                      Memory  = OutM(df))
+    if (i == 1) res <- row else res <- rbind(res, row)
+    i <- i + 1
+  }
+  res
 }
 
-Stats <- function(df, col="states", comment="") {
-  # Get statistics split up by the 110 transition/acceptance density classes
+Michel <- function(list, col="states", labels=names(list)) {
+  # Combine resulrs for Michel automata in single data frame
+  # Args: list: a list of data frames
+  #       col:  name of the column to display in the result
+  #       labels: character vector with a label for each data frame in 'list'
+  # Returns: a data frame with one row for each data frame in 'list'
   #----------------------------------------------------------------------------#
-  # Transition densities (11). Don't use seq(1, 3, 0.2), or '==' will break!
+  i <- 1
+  for (df in list) {
+    row <- data.frame(Version = labels[i],
+                      `Michel 1` = df[1, col],
+                      `Michel 2` = df[2, col],
+                      `Michel 3` = df[3, col],
+                      `Michel 4` = df[4, col], check.names=FALSE)
+    if (i == 1) res <- row else res <- rbind(res, row)
+    i <- i + 1
+  }
+  res
+}
+
+Stats <- function(list, col="states", labels=names(list)) {
+  # Get statistics aggregated over entire data frame for a list of data frames
+  # Args: list:   a list of data frames
+  #       col:    name of the column for which to calculate the statistics
+  #       labels: character vector with a label for each data frame in 'list'
+  # Returns: a data frame with the stats with one row for each input data frame
+  #----------------------------------------------------------------------------#
+  i <- 1
+  for (df in list) {
+    x <- df[[col]]
+    row <- data.frame(Version = labels[i],
+                      Mean    = mean(x),
+                      Min.    = min(x),
+                      P25     = quantile(x, 0.25, names=FALSE),
+                      Median  = median(x),
+                      P75     = quantile(x, 0.75, names=FALSE),
+                      Max.    = max(x))
+    if (i == 1) res <- row else res <- rbind(res, row)
+    i <- i + 1
+  }
+  res
+}
+
+StatsGoal <- function(df, col="states") {
+  # Get statistics split up by the 110 transition/acceptance density classes
+  # Args: df:  a data frame with effective samples from the GOAL test set
+  #       col: name of the column for which to calculate the statistics
+  # Returns: a data frame with one row of stats for each of the dt/da classes
+  #----------------------------------------------------------------------------#
+  # Transition and acceptance densities. Don't create with 'seq', or '==' fails.
   dt <- c(1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0)
-  # Acceptance densities (10). Don't use seq(0.1, 1, 0.1), or '==' will break!
   da <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
-  i <- 0
+  i <- 1
   for (t in dt) {
     for (a in da) {
-      s <- with(df, df[dt == t & da == a & !is.na(states), col])
-      row <- data.frame(mean=mean(s),
-                        min=min(s),
-                        p25=as.numeric(quantile(s, 0.25)),
-                        median=median(s),
-                        p75=as.numeric(quantile(s, 0.75)),
-                        max=max(s),
-                        dt=t,
-                        da=a,
-                        n=length(s),
-                        comments=comment)
-      if (i == 0) stats <- row               else
-                  stats <- rbind(stats, row)
+      x <- df[df$dt == t & df$da == a, col]
+      row <- data.frame(mean   = mean(x),
+                        min    = min(x),
+                        p25    = quantile(x, 0.25, names=FALSE),
+                        median = median(x),
+                        p75    = quantile(x, 0.75, names=FALSE),
+                        max    = max(x),
+                        dt     = t,
+                        da     = a,
+                        n      = length(x))
+      if (i == 1) res <- row else res <- rbind(res, row)
       i <- i + 1
     }
   }
-  stats
+  res
 }
 
-StatsMatrix <- function(df, col="median") {
-  # Arrange one statistics from 'Stats2' as a matrix with 11 transition density
-  # rows (dt), and 10 acceptance density columns (da)
+GoalMatrix <- function(x, col1="states", col2="median") {
+  # Print one statistics from 'StatsCls' as a matrix with the transition densi-
+  # ties as rows and the acceptance densities as columns
   # Args:
-  #   df:  output data frame of 'Stats2'
-  #   col: column name of one of the columns of 'df'
+  #   df:  a data frame with results from the GOAL test set
+  #   col1: name of a column of 'df'
+  #   col2: the stat to print as a matrix, column name or result of 'StatsCls'
+  # Returns: a matrix
   #----------------------------------------------------------------------------#
-  dt <- Float(seq(1, 3, 0.2), d=1)
-  da <- Float(seq(0.1, 1, 0.1), d=1)
-  # round(df[[col]], digits=1)
-  matrix(df[[col]], nrow=11, ncol=10, dimnames=list(dt, da), byrow=TRUE)
-}
-
-FormatDataFrame <- function(df, digits=1) {
-  # Format the number columns (int or float) in a data frame for printing
-  #----------------------------------------------------------------------------#
+  if (is.data.frame(x)) { x <- list(tmp=x); no.list <- TRUE } else no.list <- FALSE
+  # Row and column names
+  dt <- Float(seq(1, 3, 0.2), d=1)    # Rows
+  da <- Float(seq(0.1, 1, 0.1), d=1)  # Columns
+  n <- names(x)
+  l <- list()
   i <- 1
-  for (col in df) {
-    if (class(col) == "numeric") df[[i]] <- Float(col, d=digits) else
-    if (class(col) == "integer") df[[i]] <- Int(col)
+  for (df in x) {
+    stats.goal <- StatsGoal(df, col=col1)
+    name <- n[i]
+    l[[name]] <- matrix(stats.goal[[col2]], nrow=11, ncol=10, dimnames=list(dt, da), byrow=TRUE)
     i <- i + 1
   }
-  df
+  if (no.list) l[[1]] else l
 }
 
-LatexTable <- function(x, digits=NULL, align=NULL, ...) {
-  # Transform a R data frame or matrix into a LaTeX table
+GoalInputMatrix <- function(df, col) {
+  # Transition and acceptance densities. Don't create with 'seq', or '==' fails.
+  dt <- c(1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0)
+  da <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
+  vals <- integer()
+  for (t in dt) {
+    for (a in da) {
+      vals <- c(vals, nrow(df[df$dt == t & df$da == a & df[[col]] == "Y",]))
+    }
+  }
+  matrix(vals, nrow=11, ncol=10, dimnames=list(Float(dt, d=1), Float(da, d=1)), byrow=TRUE)
+}
+
+# GoalMatrix <- function(df, col1="states", col2="median") {
+#   # Print one statistics from 'StatsCls' as a matrix with the transition densi-
+#   # ties as rows and the acceptance densities as columns
+#   # Args:
+#   #   df:  a data frame with results from the GOAL test set
+#   #   col1: name of a column of 'df'
+#   #   col2: the stat to print as a matrix, column name or result of 'StatsCls'
+#   # Returns: a matrix
+#   #----------------------------------------------------------------------------#
+#   stats.goal <- StatsGoal(df, col=col1)
+#   dt <- Float(seq(1, 3, 0.2), d=1)    # Rows
+#   da <- Float(seq(0.1, 1, 0.1), d=1)  # Columns
+#   matrix(stats.goal[[col2]], nrow=11, ncol=10, dimnames=list(dt, da), byrow=TRUE)
+# }
+
+LatexTable <- function(x, format="f", digits=1, align=NULL, ...) {
+  # Transform a data frame or matrix to a LaTeX table
+  # Args: x:      data frame or matrix
+  #       format: character vector with "f", "d", or "" for each column of a
+  #               data frame, or just a single "f", "d", or ""
+  #       digits: number of digits for numbers falling under "f" formatting
+  #       align:  LaTeX column specification with one extra column for row names
+  #       ...:    further arguments for 'print.xtable'
+  # Returns: writes LaTeX code to the output device
   #----------------------------------------------------------------------------#
+  # If 'align' is not specified, set it explicitely to not leave it to 'xtable'
+  if (is.null(align)) align.auto <- "r"  # Extra column for rownames
+
+  # Formatting data frame
+  if (is.data.frame(x)) {
+    if (length(format) == 1) format <- rep_len(format, ncol(x))
+    i <- 1
+    for (col in x) {
+      if (class(col) == "numeric" | class(col) == "integer") {
+        # Format 'numeric' and 'integer' columns, result is 'character'
+        if (format[i] == "f") x[[i]] <- Float(col, d=digits) else
+        if (format[i] == "d") x[[i]] <- Int(col)
+        # Even though the column is now 'character', we want it right-aligned
+        if (is.null(align)) align.auto <- paste0(align.auto, "r")
+      }
+      # If the column is 'character' or 'factor'
+      else {
+        if (is.null(align)) align.auto <- paste0(align.auto, "l")
+      }
+      i <- i + 1
+    }
+    
+  } else
+
+  # Formatting matrix
+  if (is.matrix(x)) {
+    if (format[1] == "f") x <- Float(x, d=digits) else
+    if (format[1] == "d") x <- Int(x)
+    # All values in a matrix right-aligned
+    if (is.null(align))
+      for (i in seq(1, ncol(x))) align.auto <- paste0(align.auto, "r")
+  }
+  else stop("Argument 'x' must be data frame or matrix, found ", class(x))
+
+  if (is.null(align)) align <- align.auto
+
+  # Create the LaTeX code
   library(xtable)
-  print(xtable(x, digits=digits, align=align), ...)
-}
-
-StatsMatrix2Latex <- function(m, digits=NULL, file="") {
-  # Transform the output matrix of 'StatsMatrix' to a LaTeX table. The only
-  # difference to 'Latex' is that a vertical line is added after first column.
-  #----------------------------------------------------------------------------#
-  Latex(m, digits=digits, align="r|rrrrrrrrrr", file=file)
+  print(xtable(x, align=align), ...)
 }
 
 EffSamples <- function(list, vector=FALSE) {
@@ -136,6 +247,13 @@ EffSamples <- function(list, vector=FALSE) {
     i <- i + 1
   }
   if (vector) v else lapply(list, `[`, v, )
+}
+
+EffSamplesNumber <- function(list, invert=FALSE) {
+  # Get the number of effective samples from a list of data frames
+  #----------------------------------------------------------------------------#
+  n <- nrow(EffSamples(list)[[1]])
+  if (invert) nrow(list[[1]]) - n else n
 }
 
 Contour <- function(m, grid=TRUE, ...) {
@@ -179,19 +297,6 @@ Persp <- function(m, theta=0, ...) {
   # Note: persp plots in Christian's thesis: dt/da matrix and theta=-135
   persp(x=x, y=y, z=z, ticktype=ticktype, nticks=nticks, phi=phi, theta=theta,
         expand=expand, d=d, shade=shade, ...)
-}
-
-# Format a vector of numbers as integers or floats, return them as 'character'
-Int   <- function(n)      { formatC(n, format="d", big.mark=",") }
-Float <- function(n, d=2) { formatC(n, format="f", big.mark=",", digits=d) }
-
-
-Complexity <- function(n, m) {
-  # Write two numbers n and m in the form m = (xn)^n
-  #----------------------------------------------------------------------------#
-  x <- m^(1/n)/n  # x = nth_root(m)/n
-  x <- round(x, digits=3)
-  write(paste0("(",x,"n)^n"), file="")
 }
 
 # These functions can be used to draw custom axes, if Persp has been called with
@@ -272,4 +377,17 @@ PerspAxisLabel <- function(a, label="Move me", ...) {
   }
   outer <- trans3d(x, y, z, pmat)
   list(inner=inner, outer=outer)
+}
+
+# Format a vector of numbers as integers or floats, return them as 'character'
+Int   <- function(n)      { formatC(n, format="d", big.mark=",") }
+Float <- function(n, d=2) { formatC(n, format="f", big.mark=",", digits=d) }
+
+
+Complexity <- function(n, m) {
+  # Write two numbers n and m in the form m = (xn)^n
+  #----------------------------------------------------------------------------#
+  x <- m^(1/n)/n  # x = nth_root(m)/n
+  x <- round(x, digits=3)
+  write(paste0("(",x,"n)^n"), file="")
 }
