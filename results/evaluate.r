0c3e52d4264f5aeb849e973482039fd31099fa30
Daniel Weibel <daniel.weibel@unifr.ch>
2015-06-16 23:10:04 +0200

    Report

--- results/evaluate.r
+++ results/evaluate.r
@@ -3,9 +3,9 @@ GetData <- function() {
   SetDir()
 
   # Test set analysis
-  cmpl <<- ReadSingle("completeness/completeness.csv")
-  univ <<- ReadSingle("universality/universality.csv")
-  empt <<- ReadSingle("emptiness/emptiness.csv")
+  cmpl <<- ReadSingle("testset/completeness/completeness.csv")
+  univ <<- ReadSingle("testset/universality/universality.csv")
+  empt <<- ReadSingle("testset/emptiness/emptiness.csv")
   
   # Internal: GOAL test set
   i.g.all <<- ReadSubdirs("internal/goal")[c(1, 8, 7, 2, 3, 5, 4, 6)]
@@ -33,7 +33,6 @@ All <- function() {
   IntMichel()
   ExtGoal()
   ExtMichel()
-  Appendices()
 }
 
 GoalTestSet <- function() {
@@ -76,20 +75,21 @@ GoalTestSet <- function() {
 IntGoal <- function() {
   dir <- .MkDir("internal/goal")
 
-  # LaTeX table with timeouts and memory outs
+  # Table with timeouts and memory outs
   file <- paste0(dir, "/outs.tex")
   df <- Outs(i.g.all)
   LatexTable(df, format="d", file=file)
 
-  # LaTeX table with aggregated statistics of states (effective samples)
+  # Table with statistics of states
   file <- paste0(dir, "/s.stats.tex")
   df <- Stats(i.g)
   LatexTable(df, format=c("", "f", "d", "f", "f", "f", "d"), file=file)
 
-  # LaTeX table with aggregated statistics of times (effective samples)
+  # Table with statistics of times
   file <- paste0(dir, "/t.stats.tex")
-  df <- Stats(i.g, dat="cpu_time")
-  LatexTable(df, format="f", digits=2, file=file)
+  stats <- Stats(i.g, dat="cpu_time", total=TRUE, total.hours=TRUE)
+  format <- c("", "f", "f", "f", "f", "f", "f", "f", "d")
+  LatexTable(stats, format=format, digits=1, file=file)
 
   # Strip chart of states (including decimation to make PDF lighter)
   file <- paste0(dir, "/s.stripchart.pdf")
@@ -101,7 +101,17 @@ IntGoal <- function() {
   pdf(file=file, width=8, height=5)
   par(mar=c(3.5, 9.25, 0.5, 0.5))
   Stripchart(i.g.light, xlim=c(0, 60000), jitter=0.275)
-  text(x=30000, y=-1, labels="Complement size (states)", xpd=TRUE)
+  text(x=30000, y=-1, labels="Complement size", xpd=TRUE)
+  dev.off()
+
+  # Strip chart of times (including decimation)
+  file <- paste0(dir, "/t.stripchart.pdf")
+  i.g.light <- .Decimate(i.g, 10, 0.5, dat="cpu_time")
+  i.g.light <- .Decimate(i.g.light, 25, 0.5, dat="cpu_time")
+  pdf(file=file, width=8, height=5)
+  par(mar=c(3.5, 9.25, 0.5, 0.5))
+  Stripchart(i.g.light, dat="cpu_time", xlim=c(0, 600), jitter=0.275)
+  text(x=300, y=-1, labels="Execution time (seconds)", xpd=TRUE)
   dev.off()
 
   # Persp plots with median of produced states
@@ -121,79 +131,83 @@ IntGoal <- function() {
   # less than 500: green; betw. 500 and 1600: yellow; more than 1600: red
   Image(m.avg, breaks=c(0, 500, 1600, 6000), col=c("green", "yellow", "red"))
   dev.off()
-}
 
-IntMichel <- function() {
-  dir <- .MkDir("internal/michel")
-
-  # Table with the complement sizes
-  file <- paste0(dir, "/s.table.tex")
-  LatexTable(MichelTable(i.m), format="d", align="rlrrrrrr", file=file)
-
-  # Barplot with complement sizes
-  file <- paste0(dir, "/s.barplot.pdf")
-  pdf(file=file, width=7, height=5)
-  par(mar=c(7.5, 6, 1, 0.125))
-  MichelBarplot(i.m, ylim=c(0, 300000), col="white", lin=TRUE)
-  text(x=-6.75, y=150000, labels="Complement size", srt=90, xpd=TRUE)
-  dev.off()
-
-  # Barplot with execution times
-  file <- paste0(dir, "/t.barplot.pdf")
-  pdf(file=file, width=7, height=5)
-  par(mar=c(7.5, 6, 1, 0.125))
-  MichelBarplot(i.m, dat="cpu_time", col="white", lin=TRUE)
-  text(x=-6.75, y=50000, labels="Running time (sec.)", srt=90, xpd=TRUE)
-  dev.off()
+  # Matrices corresponding to all the persp plots (in separate folder)
+  dir <- .MkDir("internal/goal/matrices")
+  m.lst <- MatrixGoal(i.g)
+  .Matrices(m.lst, dir)
 }
 
 ExtGoal <- function() {
-dir <- .MkDir("external/goal")
+  dir <- .MkDir("external/goal")
   
-  # LaTeX table with timeouts and memory outs (with Rank)
+  # Table with timeouts and memory outs (with Rank)
   file <- paste0(dir, "/outs.tex")
   df <- Outs(e.g.with_rank.all)
   LatexTable(df, format="d", file=file)
-  
-  # Stripchart with complement sizes (effective samples with Rank)
-  file <- paste0(dir, "/s.stripchart.with_rank.pdf")
-  e.g.with_rank.light <- .Decimate(e.g.with_rank, 1000, 0.8)
-  pdf(file=file, width=8, height=5)
-  par(mar=c(2.25, 10.25, 0.5, 0.5))
-  Stripchart(e.g.with_rank.light)
-  dev.off()
 
-  # LaTeX table with aggregated stats of states (effective samples with Rank)
+  # Table with statistics of states (with Rank -> 7,204 effective samples)
   file <- paste0(dir, "/s.stats.with_rank.tex")
   df <- Stats(e.g.with_rank)
   LatexTable(df, format=c("", "f", "d", "f", "f", "f", "d"), file=file)
 
-  # LaTeX table with aggregated stats of times (effective samples with Rank)
+  # Table with statistics of times (with Rank -> 7,204 effective samples)
   file <- paste0(dir, "/t.stats.with_rank.tex")
-  df <- Stats(e.g.with_rank, dat="cpu_time")
-  LatexTable(df, format="f", digits=2, file=file)
+  stats <- Stats(e.g.with_rank, dat="cpu_time", total=TRUE, total.hours=TRUE)
+  format <- c("", "f", "f", "f", "f", "f", "f", "f", "d")
+  LatexTable(stats, format=format, digits=1, file=file)
 
-  # Stripchart with complement sizes (effective samples without Rank)
-  file <- paste0(dir, "/s.stripchart.pdf")
-  e.g.light <- .Decimate(e.g, 500, 0.8)
-  e.g.light <- .Decimate(e.g.light, 2000, 0.3)
-  e.g.light <- .Decimate(e.g.light, 3000, 0.1)
-  pdf(file=file, width=8, height=5)
-  par(mar=c(2.25, 10.25, 0.5, 0.5))
-  Stripchart(e.g.light)
-  dev.off()
-  
-  # LaTeX table with aggregated stats of states (effective samples without Rank)
+  # Table with statistics of states (without Rank -> 10,998 effective samples)
   file <- paste0(dir, "/s.stats.tex")
   df <- Stats(e.g)
   LatexTable(df, format=c("", "f", "d", "f", "f", "f", "d"), file=file)
 
-  # LaTeX table with aggregated stats of times (effective samples without Rank)
+  # Table with statistics of times (without Rank -> 10,998 effective samples)
   file <- paste0(dir, "/t.stats.tex")
-  df <- Stats(e.g, dat="cpu_time")
-  LatexTable(df, format="f", digits=2, file=file)
+  stats <- Stats(e.g, dat="cpu_time", total=TRUE, total.hours=TRUE)
+  format <- c("", "f", "f", "f", "f", "f", "f", "f", "d")
+  LatexTable(stats, format=format, file=file, digits=1)
 
-  # Persp plots with median of produced states
+  # Stripchart of complement sizes (with Rank -> 7,204 effective samples)
+  file <- paste0(dir, "/s.stripchart.with_rank.pdf")
+  e.g.with_rank.light <- .Decimate(e.g.with_rank, 1000, 0.8)
+  pdf(file=file, width=8, height=4)
+  par(mar=c(3.5, 10.25, 0.5, 0.5))
+  Stripchart(e.g.with_rank.light, jitter=0.275)
+  text(x=60000, y=-0.45, labels="Complement size", xpd=TRUE)
+  dev.off()
+
+  # Stripchart of complement sizes (without Rank -> 10,998 effective samples)
+  file <- paste0(dir, "/s.stripchart.pdf")
+  e.g.light <- .Decimate(e.g, 500, 0.8)
+  e.g.light <- .Decimate(e.g.light, 2000, 0.3)
+  e.g.light <- .Decimate(e.g.light, 3000, 0.1)
+  pdf(file=file, width=8, height=3.1125)
+  par(mar=c(3.5, 10.25, 0.5, 0.5))
+  Stripchart(e.g.light, jitter=0.275)
+  text(x=20000, y=-0.45, labels="Complement size", xpd=TRUE)
+  dev.off()
+
+  # Stripchart of times (with Rank -> 7,204 effective samples)
+  file <- paste0(dir, "/t.stripchart.with_rank.pdf")
+  e.g.with_rank.light <- .Decimate(e.g.with_rank, 10, 0.5, dat="cpu_time")
+  pdf(file=file, width=8, height=4)
+  par(mar=c(3.5, 10.25, 0.5, 0.5))
+  Stripchart(e.g.with_rank.light, dat="cpu_time", xlim=c(0, 450), jitter=0.275)
+  text(x=225, y=-0.45, labels="Execution time (seconds)", xpd=TRUE)
+  dev.off()
+
+  # Stripchart of times (without Rank -> 10,998 effective samples)
+  file <- paste0(dir, "/t.stripchart.pdf")
+  e.g.light <- .Decimate(e.g, 5, 0.5, dat="cpu_time")
+  e.g.light <- .Decimate(e.g.light, 10, 0.25, dat="cpu_time")
+  pdf(file=file, width=8, height=3.1125)
+  par(mar=c(3.5, 10.25, 0.5, 0.5))
+  Stripchart(e.g.light, dat="cpu_time", xlim=c(0, 450), jitter=0.275)
+  text(x=225, y=-0.45, labels="Execution time (seconds)", xpd=TRUE)
+  dev.off()
+
+  # Persp plots with median of produced states (without Rank)
   mlist <- MatrixGoal(e.g)
   PerspBatch(mlist, pdf=TRUE, dir=dir, prefix="s.median.", width=6, height=5,
              mar=c(1, 2.5, 0, 0), zlim=c(0, 3000), z.colors=TRUE, zlab="",
@@ -201,6 +215,43 @@ dir <- .MkDir("external/goal")
              lin.xy=list(x=1, y=0.1), lin.col="gray", theta=150, phi=25,
              shade=0.5, ltheta=30, lphi=20, custom.zlab=function() {
              text(-0.73, 0.06, "States (median)", srt=94) })
+
+  # Matrices corresponding to the persp plots (without Rank)
+  dir <- .MkDir("external/goal/matrices")
+  m.lst <- MatrixGoal(e.g)
+  # Remove Fribourg, because we have it already in the internal tests
+  m.lst$`Fribourg+M1+R2C` <- NULL
+  .Matrices(m.lst, dir)
+}
+
+IntMichel <- function() {
+  dir <- .MkDir("internal/michel")
+
+  # Table with complement sizes
+  file <- paste0(dir, "/s.table.tex")
+  df <- MichelTable(i.m)
+  LatexTable(df, format="d", align="rlrrrrrr", file=file)
+
+  # Table with execution times
+  file <- paste0(dir, "/t.table.tex")
+  df <- MichelTable(i.m, dat="cpu_time")
+  LatexTable(df, align="rlrrrrrr", file=file)
+
+  # Barplot with complement sizes
+  file <- paste0(dir, "/s.barplot.pdf")
+  pdf(file=file, width=7, height=5)
+  par(mar=c(7.5, 6, 1, 0.125))
+  MichelBarplot(i.m, ylim=c(0, 300000), col="white", lin=TRUE)
+  text(x=-5.5, y=150000, labels="Complement size", srt=90, xpd=TRUE)
+  dev.off()
+
+  # Barplot with execution times
+  file <- paste0(dir, "/t.barplot.pdf")
+  pdf(file=file, width=7, height=5)
+  par(mar=c(7.5, 6, 1, 0.125))
+  MichelBarplot(i.m, dat="cpu_time", col="white", lin=TRUE)
+  text(x=-5.5, y=50000, labels="Running time (sec.)", srt=90, xpd=TRUE)
+  dev.off()
 }
 
 
@@ -209,65 +260,35 @@ ExtMichel <- function() {
 
   # Table with the complement sizes
   file <- paste0(dir, "/s.table.tex")
-  LatexTable(MichelTable(e.m), format="d", align="rlrrrrrr", file=file)
+  df <- MichelTable(e.m)
+  LatexTable(df, format="d", align="rlrrrrrr", file=file)
 
   # Table with the execution times
   file <- paste0(dir, "/t.table.tex")
-  LatexTable(MichelTable(e.m), dat="cpu_time", align="rlrrrrrr", file=file)
+  df <- MichelTable(e.m, dat="cpu_time")
+  LatexTable(df, align="rlrrrrrr", file=file)
 
   # Barplot with complement sizes
   file <- paste0(dir, "/s.barplot.pdf")
   pdf(file=file, width=7, height=5)
   par(mar=c(8, 6, 1, 0.125))
   MichelBarplot(e.m, ylim=c(0, 180000), yaxp=c(0, 180000, 9), col="white", lin=TRUE)
-  text(x=-3.5, y=100000, labels="Complement size", srt=90, xpd=TRUE)
+  text(x=-3.25  , y=100000, labels="Complement size", srt=90, xpd=TRUE)
   dev.off()
 
-  # No barplot for execution times, because the value for Piterman (Michel 4)
-  # is so high that the other values cannot be distinguished anymore
+  # Barplot with execution times
+  file <- paste0(dir, "/t.barplot.pdf")
+  pdf(file=file, width=7, height=5)
+  par(mar=c(8, 6, 1, 0.125))
+  MichelBarplot(e.m, dat="cpu_time", ylim=c(0, 80000), col="white", lin=TRUE)
+  text(x=-3.25, y=40000, labels="Running time (sec.)", srt=90, xpd=TRUE)
+  dev.off()
 }
 
-Appendices <- function() {
-  dir <- .MkDir("appendices")
 
-  # Matrices of median complement sizes of internal tests
-  m.lst <- MatrixGoal(i.g)
-  .AppendixMatrices(m.lst, dir)
-
-  # Matrices of median complement sizes of external tests (without Rank)
-  m.lst <- MatrixGoal(e.g)
-  # Remove Fribourg, because we have it already in the internal tests
-  m.lst$`Fribourg+M1+R2C` <- NULL
-  .AppendixMatrices(m.lst, dir)
-
-  # CPU time stats for GOAL test set (internal)
-  file <- paste0(dir, "/i.g.t.stats.tex")
-  stats <- Stats(i.g, dat="cpu_time", total=TRUE, total.hours=TRUE)
-  format <- c("", "f", "f", "f", "f", "f", "f", "f", "d")
-  LatexTable(stats, format=format, file=file, digits=1)
-
-  # CPU time stats for GOAL test set (external with Rank -> 7,204 eff. samples)
-  file <- paste0(dir, "/e.g.t.stats.with_rank.tex")
-  stats <- Stats(e.g.with_rank, dat="cpu_time", total=TRUE, total.hours=TRUE)
-  format <- c("", "f", "f", "f", "f", "f", "f", "f", "d")
-  LatexTable(stats, format=format, file=file, digits=1)
-
-  # CPU time stats for GOAL test set (external without Rank -> 10,998 eff. samples)
-  file <- paste0(dir, "/e.g.t.stats.without_rank.tex")
-  stats <- Stats(e.g, dat="cpu_time", total=TRUE, total.hours=TRUE)
-  format <- c("", "f", "f", "f", "f", "f", "f", "f", "d")
-  LatexTable(stats, format=format, file=file, digits=1)
-
-  # CPU times for Michel test set (internal)
-  file <- paste0(dir, "/i.m.t.tex")
-  LatexTable(MichelTable(i.m, dat="cpu_time"), align="rlrrrrrr", file=file)
-
-  # CPU times for Michel test set (external)
-  file <- paste0(dir, "/e.m.t.tex")
-  LatexTable(MichelTable(e.m, dat="cpu_time"), align="rlrrrrrr", file=file)
-}
-
-.AppendixMatrices <- function(m.lst, dir) {
+.Matrices <- function(m.lst, dir) {
+  # Print the matrices of the persp plots as LaTeX tables for the appendix
+  #----------------------------------------------------------------------------#
   i <- 1
   for (m in m.lst) {
     file <- paste0(dir, "/s.median.", names(m.lst)[i], ".tex")
@@ -281,13 +302,13 @@ Appendices <- function() {
 }
 
 
-.Decimate <- function(list, threshold, rm.perc) {
+.Decimate <- function(list, threshold, rm.perc, dat="states") {
   # Remove 'rm.perc' percent of the rows with less than 'threshold' states from
   # the data frames in 'list'
   #----------------------------------------------------------------------------#
   lapply(list, function(df) {
-    rows <- rownames(df[df$states > threshold,])  # Rows to keep
-    x <- rownames(df[df$states <= threshold,])    # Candidate rows to remove
+    rows <- rownames(df[df[[dat]] > threshold,])  # Rows to keep
+    x <- rownames(df[df[[dat]] <= threshold,])    # Candidate rows to remove
     rows <- c(rows, sample(x, size=round((1 - rm.perc) * length(x))))
     df[rows,]
   })
