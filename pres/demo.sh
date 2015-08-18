1bbde5a3b42aba60e9ee0ffa8649a0f2bc7dd4f8
Daniel Weibel <daniel.weibel@unifr.ch>
2015-08-18 23:42:35 +0200

    Presentation

---
+++ pres/demo.sh
@@ -0,0 +1,4 @@
+# Prepare shell for demo
+cd /Applications/GOAL.app/Contents/Resources/Java
+history -s ./gc complement -m fribourg -r2c -m1 -m2 '~/Desktop/in.gff'
+history -s ./gc complement -m piterman '~/Desktop/in.gff'
